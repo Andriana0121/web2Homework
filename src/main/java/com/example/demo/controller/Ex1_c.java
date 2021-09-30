c1d4d6be9b032b36204ab916e729650f8a372452
Andriana0121 <vorotna.andriana01@gmail.com>
2021-09-30 19:43:36 +0300

    initial commit

---
+++ src/main/java/com/example/demo/controller/Ex1_c.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public  class Ex1_c {
+    @GetMapping("ex1c")
+    public String method3(HttpServletRequest httpServletRequest) {
+        return "URL-ul absolut al cererii HTTP -> getRequestURL: " + httpServletRequest.getRequestURL();
+    }
+}
